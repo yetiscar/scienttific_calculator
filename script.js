ef7592cf47af5464112447924ccb0cc34f67db77
NIKHIL TIRKEY <106531539+yetiscar@users.noreply.github.com>
2023-09-06 12:32:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let string = "";
+let Buttons = document.querySelectorAll('.Button');
+Array.from(Buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
